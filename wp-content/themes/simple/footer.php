b8b00ab8944995ca37aabd00df6d7cea386d088b
miwanoff <marina.iva.noffa@gmail.com>
2021-12-05 13:40:32 +0200

    add code to footer.php

---
+++ wp-content/themes/simple/footer.php
@@ -0,0 +1,13 @@
+  <!-- Подвал сайта -->
+  <footer id="colophon" class="site-footer">
+      <div class="site-info">
+          <a href="<?php echo esc_url(__('http://wordpress.org/')); ?>">
+              <?php echo 'KIT 2020'; ?>
+          </a>
+      </div><!-- .site-info -->
+      <?php wp_footer();?>
+  </footer><!-- #colophon -->
+  </div><!-- #page -->
+  </body>
+
+  </html>
