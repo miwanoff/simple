926ec5ca19a5b4d4c564131817420ad9be910ba4
miwanoff <marina.iva.noffa@gmail.com>
2021-12-05 13:37:27 +0200

    add sidebar.php and searchform.php

---
+++ wp-content/themes/simple/searchform.php
@@ -0,0 +1,5 @@
+<form name="search" action="<?php echo home_url('/') ?>" method="get" class="search-form">
+    <input type="text" value="<?php echo get_search_query() ?>" name="s" placeholder="<?php echo 'Поиск'; ?>"
+        class="input">
+    <button type="submit" class="button"><?php echo 'Найти'; ?></button>
+</form>
---
+++ wp-content/themes/simple/sidebar.php
@@ -0,0 +1,8 @@
+<aside class="sidebar ">
+    <div id="search-container">
+        <div class="search-box">
+            <?php get_search_form();?>
+        </div>
+    </div>
+    <!-- Меню -->
+</aside>
